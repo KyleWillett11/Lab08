12327b760ead6fc6a0694bcbf2376ba865105e80
Kyle Willett <kyle_willett11@uri.edu>
2022-03-24 17:44:04 -0400

    Question3: Commit and push python code to github repo

---
+++ CSC434-lab08.py
@@ -0,0 +1,7 @@
+userName = input("Hello! Please enter your Facebook username!: ")
+userPassword = input("Hello! Please enter your Facebook Password!: ")
+
+if(userName.lower() =="kyle willett" and userPassword == "Pa55word"):
+    print("Welcome to Facebook")
+else:
+    print("Wrong credentials, try again")
